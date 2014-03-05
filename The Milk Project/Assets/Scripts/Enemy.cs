8faad016bcea58433f6583db7e3dbda4ef120507
Trevor Tang <tangt07@gmail.com>
2014-03-05 13:57:51 -0600

    Enemy script holds damage information used by playerCollisions

---
+++ The Milk Project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+	public float damage = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
