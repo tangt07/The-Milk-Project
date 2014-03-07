2e0a41132593f5feefbf0680ef4bd3178416bf61
Trevor Tang <tangt07@gmail.com>
2014-03-07 01:51:44 -0600

    Camera Movement script"

---
+++ The Milk Project/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+	public GameObject player;
+	public float minsize=2f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 playerposition = player.transform.position;
+		if(playerposition.y > 2f*minsize-3f){
+		transform.position = new Vector3(playerposition.x,.5f*playerposition.y-.5f,-10);
+		camera.orthographicSize = .5f*playerposition.y+1.5f;
+		}
+		else{
+			transform.position = new Vector3(playerposition.x,minsize-2,-10);
+			camera.orthographicSize = minsize;
+
+		}
+		//size position maxheightplayer
+		//2 0 1
+		//3 1 3
+		//4 2 4.9
+		//5 3 7
+	}
+}
