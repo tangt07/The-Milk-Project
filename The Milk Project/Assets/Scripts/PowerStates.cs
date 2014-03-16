b8b1bccbe7f41aa3e3588d984cd2ce31a1713db9
Trevor Tang <tangt07@gmail.com>
2014-03-16 14:05:52 -0500

    remove powerstate variable, redundant

--- The Milk Project/Assets/Scripts/PowerStates.cs
+++ The Milk Project/Assets/Scripts/PowerStates.cs
@@ -5,12 +5,10 @@ public class PowerStates : MonoBehaviour {
 	public int powerstate=0;
 	public Collider2D Strawberry;
 	public Collider2D Chocolate;
-	private Animator strawberryanim, chocolateanim, anim;
+	private Animator anim;
 	Health playerhealth;
 	// Use this for initialization
 	void Start () {
-		strawberryanim = Strawberry.gameObject.GetComponent <Animator> (); 		// Reference to the player's animator component.
-		chocolateanim = Chocolate.gameObject.GetComponent <Animator> (); 		// Reference to the player's animator component.
 		anim = GetComponent <Animator> (); 		// Reference to the player's animator component.
 
 		playerhealth = GetComponent<Health>();
@@ -26,17 +24,13 @@ public class PowerStates : MonoBehaviour {
 		if (collision.collider == Chocolate) {
 			Debug.Log("hit chocolate");
 			playerhealth.invulnerable = true;			
-			powerstate=1;
 			anim.SetTrigger("PowerUpNC");
-			anim.SetInteger("PowerState", 1);
-			
+
 		}
 		if (collision.collider == Strawberry) {
 			Debug.Log("hit strawberry");
 			playerhealth.invulnerable = true;		
-			powerstate=2;
 			anim.SetTrigger("PowerUpNS");
-			anim.SetInteger("PowerState", 2);
 
 		}
 
